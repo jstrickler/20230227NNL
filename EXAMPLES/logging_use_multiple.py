14a417e1e1abb35c960f51546f89a01bd2b3f199
jstrickler <jstrickler@gmail.com>
2023-03-01 16:09:53 -0500

    added extra logging examples

---
+++ EXAMPLES/logging_use_multiple.py
@@ -0,0 +1,8 @@
+
+from logging_multiple import debug, info, warning, error, critical
+
+debug('debug message')
+info('info message')
+warning('warning message')
+error('error message')
+critical('critical message')
